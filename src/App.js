b94a47b3ea6e2a4c67f2e76813868fe322c30e83
Ruben Petit <rapetitc@gmail.com>
2025-02-27 16:57:41 -0300

    fix: :art: Adding "Private" and "Only Public" routes, and removing Category section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { Route, Routes } from "react-router-dom";
+import { useContext } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 import Home from "./Pages/Home";
@@ -9,27 +10,37 @@ import Sell from "./Pages/Sell";
 import ItemDetailContainer from "./Pages/ItemDetailContainer";
 import Search from "./Pages/Search";
 import Categories from "./Pages/Categories";
-import Category from "./Pages/Category";
 import Cart from "./Pages/Cart";
 import Ticket from "./Pages/Ticket";
 import NotFoundPage from "./Pages/NotFoundPage";
+import AuthenticatorContext from "./Context/AuthenticatorContext";
 
 function App() {
-  // TODO Encerrar enlaces publicos, y privados
+  const { token } = useContext(AuthenticatorContext);
+
   return (
     <Routes>
       <Route path="/" element={<Home />}></Route>
-      <Route path="/create-account/*" element={<CreateAccount />}></Route>
-      <Route path="/login" element={<LogIn />}></Route>
-      <Route path="/profile" element={<Profile />}></Route>
-      <Route path="/sell" element={<Sell />}></Route>
       <Route path="/product/:id" element={<ItemDetailContainer />}></Route>
       <Route path="/search" element={<Search />}></Route>
       <Route path="/categories" element={<Categories />}></Route>
-      <Route path="/category/:id" element={<Category />}></Route>
       <Route path="/cart" element={<Cart />} />
-      <Route path="/ticket/:id" element={<Ticket />} />
-      <Route path="*" element={<NotFoundPage />}></Route>
+      {!token ? (
+        <>
+          {/* Only PUBLIC links */}
+          <Route path="/create-account/*" element={<CreateAccount />}></Route>
+          <Route path="/login" element={<LogIn />}></Route>
+        </>
+      ) : (
+        <>
+          {/* Only PRIVATE links */}
+          <Route path="/profile" element={<Profile />}></Route>
+          <Route path="/sell" element={<Sell />}></Route>
+          <Route path="/ticket/:id" element={<Ticket />} />
+        </>
+      )}
+      <Route path="/not-found" element={<NotFoundPage />}></Route>
+      <Route path="*" element={<Navigate to={"/not-found"} />}></Route>
     </Routes>
   );
 }
