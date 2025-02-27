f318c918716980062d538be0be072f27357cf27a
Ruben Petit <rapetitc@gmail.com>
2025-02-27 16:59:21 -0300

    style: :lipstick: Updating Profile Nav styles

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -19,7 +19,7 @@ const NavBar = () => {
         <div className="col-start-4 col-span-5 self-center">
           <Menu />
         </div>
-        <div className="col-span-4 flex justify-end items-center gap-5 min-w-max py-1 mx-4">
+        <div className="col-span-4 flex justify-end items-center gap-6 min-w-max py-1 mx-4">
           <ProfileNav />
           <ShoppingCart />
         </div>
--- src/Components/ProfileNav.js
+++ src/Components/ProfileNav.js
@@ -6,34 +6,58 @@ import AuthenticatorContext from "../Context/AuthenticatorContext";
 const ProfileNav = () => {
   const { token, closeSession } = useContext(AuthenticatorContext);
 
-  return token === null ? (
-    <div className="flex gap-4">
-      <Link to="/create-account" className="hover:underline">
-        Crear una cuenta
-      </Link>
-      <Link to="/login" className="hover:underline">
-        Iniciar Sesion
-      </Link>
-    </div>
-  ) : (
-    <div className="flex flex-col relative gap-4 rounded bg-white group">
-      {/* TODO Mejorar UI, considerar poner el enlace hacia "VENDER" */}
-      <button className="px-2 py-1 hover:cursor-pointer">
-        Bienvenido {token.user.fname}!
-      </button>
-      <div className="flex flex-col items-end gap-2 absolute -top-[-32px] invisible w-full px-2 py-2 bg-gray-100 group-hover:visible">
-        <Link to={"/profile"} className="hover:cursor-pointer">
-          Perfil
-        </Link>
-        <button
-          className="hover:cursor-pointer"
-          onClick={async () => {
-            await closeSession();
-          }}
-        >
-          Cerrar Sesion
-        </button>
-      </div>
+  return (
+    <div className="flex gap-6 items-center">
+      {token === null ? (
+        <>
+          <Link to="/create-account" className="hover:underline">
+            Registrarse
+          </Link>
+          <Link to="/login" className="px-3 py-1 rounded bg-white">
+            Iniciar Sesion
+          </Link>
+        </>
+      ) : (
+        <>
+          <Link to="/sell" className="hover:underline">
+            Vender
+          </Link>
+          <div className="flex flex-col hover:bg-white group">
+            <button className="flex items-center gap-2 px-3 py-1 rounded bg-white hover:cursor-pointer">
+              ¡Bienvenido {token.user.fname}!{" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                className="size-4 group-hover:-rotate-90 duration-100"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"
+                />
+              </svg>
+            </button>
+            <div className="relative invisible group-hover:visible group-hover:bg-white">
+              <div className="flex flex-col items-end gap-2 absolute w-full p-2 bg-white border-t border-gray-200">
+                <Link
+                  to={"/profile"}
+                  className="w-full px-2 py-1 text-end hover:underline"
+                >
+                  Perfil
+                </Link>
+                <button
+                  className="w-full px-2 py-1 text-end bg-gray-100 rounded hover:cursor-pointer"
+                  onClick={async () => {
+                    await closeSession();
+                  }}
+                >
+                  Cerrar Sesion
+                </button>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
