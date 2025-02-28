0c4a87e9d8a7e6672852e8807bf23e1c1909271a
Ruben Petit <rapetitc@gmail.com>
2025-02-28 04:58:01 -0300

    refactor: :sparkles: PublishedProdut & Purchases sections are connected

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,12 +1,125 @@
-import React from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { collection, getDocs, query, where } from "firebase/firestore";
 
+import { db } from "../utils/firebase_config";
+import SessionContext from "../Context/SessionContext";
 import NavBar from "../Components/Navbar";
 import Footer from "../Components/Footer";
 
-const Profile = () => {
-  //TODO Actualizar UI de Mis Compras y Productos Publicados
+const PublishedProducts = () => {
+  const { session } = useContext(SessionContext);
+  const [data, setData] = useState([]);
 
+  const getPublishedProducts = async () => {
+    const results = await getDocs(
+      query(
+        collection(db, "products"),
+        where("publisher", "==", session.user.id)
+      )
+    );
+    const products = [];
+    if (!results.empty) {
+      results.docs.forEach((product) => {
+        products.push({ id: product.id, ...product.data() });
+      });
+    }
+    setData(products);
+  };
+
+  useEffect(() => {
+    getPublishedProducts();
+  }, []);
+
+  return (
+    <div>
+      <div className="flex justify-between py-1 px-2">
+        <h4 className="text-lg font-semibold">Productos publicados</h4>
+        <Link to={"/sell"} className="px-1 hover:underline">
+          Vender
+        </Link>
+      </div>
+      {data.length == 0 ? (
+        <div>Sin productos publicados</div>
+      ) : (
+        <table className="w-full bg-white">
+          <thead className="">
+            <tr>
+              <td className="p-1 font-semibold">Producto</td>
+              <td className="p-1 font-semibold">Precio</td>
+            </tr>
+          </thead>
+          <tbody className="">
+            {data.map((data, i) => {
+              return (
+                <tr className="bg-red-100" key={i}>
+                  <td className="p-1 ">
+                    <Link>{data.title}</Link>
+                  </td>
+                  <td className="p-1 ">${data.price}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+};
+
+const Purchases = () => {
+  const { session } = useContext(SessionContext);
+  const [data, setData] = useState([]);
+
+  const getPublishedProducts = async () => {
+    const results = await getDocs(
+      query(collection(db, "tickets"), where("buyer", "==", session.user.id))
+    );
+
+    const purchases = [];
+    if (!results.empty) {
+      results.docs.forEach((purchase) => {
+        purchases.push({ id: purchase.id, ...purchase.data() });
+      });
+    }
+    setData(purchases);
+  };
+
+  useEffect(() => {
+    getPublishedProducts();
+  }, []);
+
+  return (
+    <div>
+      <div className="flex justify-between py-1 px-2">
+        <h4 className="text-lg font-semibold">Mis compras</h4>
+      </div>
+      {data.length == 0 ? (
+        <div>Sin compras realizadas</div>
+      ) : (
+        <table className="w-full bg-white">
+          <thead className="">
+            <tr>
+              <td className="p-1 font-semibold">Ticket</td>
+              <td className="p-1 font-semibold">Total pagado</td>
+            </tr>
+          </thead>
+          <tbody className="">
+            <tr className="bg-red-100">
+              <td className="p-1 ">
+                <Link>GYSGW459GTG912GOLAS25FU</Link>
+              </td>
+              <td className="p-1 ">$150</td>
+            </tr>
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+};
+
+//TODO Mejorar UI de Mis Compras y Productos Publicados
+const Profile = () => {
   return (
     <div className="flex flex-wrap content-between min-h-[100vh]">
       <div className="w-full">
@@ -17,49 +130,10 @@ const Profile = () => {
           </div>
           <div className="flex w-full p-5">
             <div className="w-full p-4">
-              <div className="flex justify-between py-1 px-2">
-                <h4 className="text-lg font-semibold">Productos publicados</h4>
-                <Link to={"/sell"} className="px-1 hover:underline">
-                  Vender
-                </Link>
-              </div>
-              <table className="w-full bg-white">
-                <thead className="">
-                  <tr>
-                    <td className="p-1 font-semibold">Producto</td>
-                    <td className="p-1 font-semibold">Precio</td>
-                  </tr>
-                </thead>
-                <tbody className="">
-                  <tr className="bg-red-100">
-                    <td className="p-1 ">
-                      <Link>Zapatos</Link>
-                    </td>
-                    <td className="p-1 ">$150</td>
-                  </tr>
-                </tbody>
-              </table>
+              <PublishedProducts />
             </div>
             <div className="w-full p-4">
-              <div className="flex justify-between py-1 px-2">
-                <h4 className="text-lg font-semibold">Mis compras</h4>
-              </div>
-              <table className="w-full bg-white">
-                <thead className="">
-                  <tr>
-                    <td className="p-1 font-semibold">Ticket</td>
-                    <td className="p-1 font-semibold">Total pagado</td>
-                  </tr>
-                </thead>
-                <tbody className="">
-                  <tr className="bg-red-100">
-                    <td className="p-1 ">
-                      <Link>GYSGW459GTG912GOLAS25FU</Link>
-                    </td>
-                    <td className="p-1 ">$150</td>
-                  </tr>
-                </tbody>
-              </table>
+              <Purchases />
             </div>
           </div>
         </div>
