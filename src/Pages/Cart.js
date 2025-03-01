39c06b5351942d2f24dcaf232e800ecae3b821c5
Ruben Petit <rapetitc@gmail.com>
2025-03-01 18:07:34 -0300

    style: :lipstick: Updating styles

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -3,18 +3,16 @@ import { Link } from "react-router-dom";
 
 import SessionContext from "../Context/SessionContext";
 import CartContext from "../Context/CartContext";
-import NavBar from "../Components/Navbar";
-import Footer from "../Components/Footer";
 
 const Cart = () => {
   const { session } = useContext(SessionContext);
   const { cart, detailedCart, getDetailedCart, handlingPay } =
     useContext(CartContext);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoaded, setIsLoaded] = useState(false);
 
   const handler = async () => {
     await getDetailedCart();
-    setIsLoading(false);
+    setIsLoaded(true);
   };
 
   useEffect(() => {
@@ -24,84 +22,91 @@ const Cart = () => {
   }, []);
 
   return (
-    <div className="flex flex-wrap content-between min-h-[100vh]">
-      <div className="w-full">
-        <NavBar />
-        <div className="w-[1280px] mx-auto">
-          <div className="flex flex-col p-4 my-5 rounded-lg bg-gray-200">
-            <h3 className="text-2xl">Carrito de compras</h3>
-          </div>
-          {cart === null || cart?.products?.length == 0 ? (
-            <div className="text-center">
-              <p>El carrito de compras está vacío</p>
-            </div>
-          ) : isLoading ? (
-            "Esta cargando"
-          ) : (
-            <div className="flex gap-10 py-2">
-              <table className="w-full h-max border border-gray-200 bg-white">
-                <thead className="">
-                  <tr>
-                    <td className="p-1 font-semibold">Producto</td>
-                    <td className="p-1 font-semibold">Cantidad</td>
-                    <td className="p-1 font-semibold">Precio</td>
-                    <td className="p-1 font-semibold">Total</td>
-                  </tr>
-                </thead>
-                <tbody className="">
-                  {detailedCart.products.map((prod, i) => {
-                    return (
-                      <tr
-                        className={
-                          i % 2 == 0 ? "bg-yellow-100" : "bg-yellow-200"
-                        }
-                        key={i}
-                      >
-                        <td className="p-1 ">
-                          <Link to={`/product/${prod.id}`}>
-                            {prod.brand} {prod.title}
-                          </Link>
-                        </td>
-                        <td className="p-1 ">{prod.quantity}</td>
-                        <td className="p-1 ">${prod.price}</td>
-                        <td className="p-1 ">${prod.price * prod.quantity}</td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
-              <div className="flex flex-col items-center gap-4 w-[300px] p-5 rounded bg-gray-100">
-                {session != null ? (
-                  <>
-                    <p className="text-lg">
-                      Total a pagar:{" "}
-                      <span className="font-semibold">
-                        ${detailedCart.total_price}
-                      </span>
-                    </p>
-                    <button
-                      className="w-full py-2 px-4 rounded bg-blue-400 cursor-pointer"
-                      onClick={() => {
-                        handlingPay();
-                      }}
-                    >
-                      Pagar
-                    </button>
-                  </>
-                ) : (
-                  <div className="text-center">
-                    <p>Antes de completar</p>
-                    <Link to={"/login"} className="hover:underline">
-                      Iniciar sesion
-                    </Link>
-                  </div>
-                )}
-              </div>
-            </div>
-          )}
-        </div>
+    <div className="w-[1280px] mx-auto">
+      <div className="flex flex-col p-4 my-5 rounded-lg bg-gray-200">
+        <h3 className="text-2xl">Carrito de compras</h3>
       </div>
-      <Footer />
+
+      {isLoaded ? (
+        cart === null || cart?.products?.length == 0 ? (
+          <div className="flex justify-center p-1">
+            <p>Carrito vacio</p>
+          </div>
+        ) : (
+          <div className="flex gap-10 py-2">
+            <table className="w-full h-max rounded-lg bg-white overflow-hidden">
+              <thead>
+                <tr>
+                  <td className="ps-15 p-2 font-semibold">Producto</td>
+                  <td className="p-2 text-center font-semibold">Cantidad</td>
+                  <td className="p-2 text-center font-semibold">Precio</td>
+                  <td className="p-2 text-center font-semibold">Total</td>
+                </tr>
+              </thead>
+              <tbody>
+                {detailedCart.products.map((prod, i) => {
+                  return (
+                    <tr
+                      className={i % 2 == 0 ? "bg-yellow-100" : "bg-yellow-200"}
+                      key={i}
+                    >
+                      <td className="p-1">
+                        <Link
+                          to={`/product/${prod.id}`}
+                          className="flex items-center gap-3 p-1 text-lg font-semibold"
+                        >
+                          <div className="flex justify-center items-center size-10 rounded bg-white  overflow-hidden">
+                            <img
+                              src={prod.images[0]}
+                              className="max-w-full max-h-full"
+                            />
+                          </div>
+                          <p>{prod.title}</p>
+                        </Link>
+                      </td>
+                      <td className="p-1 text-center">{prod.quantity}</td>
+                      <td className="p-1 text-center">${prod.price}</td>
+                      <td className="p-1 text-center">
+                        ${prod.price * prod.quantity}
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+
+            <div className="flex flex-col items-center gap-4 w-[300px] h-max p-5 rounded bg-gray-100">
+              {session != null ? (
+                <>
+                  <p className="text-lg">
+                    Total a pagar:{" "}
+                    <span className="font-semibold">
+                      ${detailedCart.total_price}
+                    </span>
+                  </p>
+                  <button
+                    className="w-full py-2 px-4 rounded bg-blue-400 cursor-pointer"
+                    onClick={() => {
+                      handlingPay();
+                    }}
+                  >
+                    Pagar
+                  </button>
+                </>
+              ) : (
+                <div className="text-center">
+                  <p>Antes de completar</p>
+                  <Link to={"/login"} className="hover:underline">
+                    Iniciar sesion
+                  </Link>
+                </div>
+              )}
+            </div>
+          </div>
+        )
+      ) : (
+        <p className="text-center">Cargando. . . </p>
+      )}
     </div>
   );
 };
